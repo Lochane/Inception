2fa13d677e7f5bedd95cfd51c3bd604734590d25
lsouquie <lsouquie@inception.myguest.virtualbox.org>
2024-11-29 18:57:33 +0100

    va te faire foutre le inception

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
+set -x
+
+chown mysql:mysql /var/lib/mysql
+chmod 755 /var/lib/mysql
+
+mkdir -p /run/mysqld 
+chown -R mysql:mysql /run/mysqld
+chmod 755 /run/mysqld
+
 # Initialisation de la base de données
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
+mysql_upgrade --datadir=/var/lib/mysql
 
-chown mysql:mysql /etc/mysql/init.sql
-chmod 600 /etc/mysql/init.sql
 
-# Démarre mysqld en premier plan et applique le fichier init.sql si présent
-if [ -f /etc/mysql/init.sql ]; then
-    exec mysqld --init-file=/etc/mysql/init.sql --user=mysql --datadir='/var/lib/mysql'
-    echo "launched mariadb with init file" > /check_mariadb/
-else
-    exec mysqld --user=mysql --datadir='/var/lib/mysql'
-    echo "launched mariadb without init file" > /check_mariadb/
-fi
+
+mysqld --datadir='/var/lib/mysql' &
+
+PID=$!
+while ! ps -p $PID > /dev/null; do
+    sleep 1
+done
+
+mysql -u root -p << EOF
+FLUSH PRIVILEGES;
+
+DELETE FROM mysql.user WHERE User = 'root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD '${DB_ROOT_PASS}';
+
+CREATE DATABASE ${DB_NAME};
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+
+FLUSH PRIVILEGES;
+EOF
+
+rm -f tmp_file
+kill $PID
+
+exec mysqld --datadir='/var/lib/mysql'
+
+# /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0
--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -5,23 +5,36 @@ chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
 
 # Copier wp-config.php
-if [ ! -f /var/www/html/wp-config.php ]; then
-  cp /wp-config.php /var/www/html/wp-config.php
-fi
+# if [ ! -f /var/www/html/wp-config.php ]; then
+#   cp /wp-config.php /var/www/html/wp-config.php
+# fi
 
 # Télécharger et configurer WordPress
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 
-if [ ! -d /var/www/html/wp-admin ]; then
-  ./wp-cli.phar core download --path=/var/www/html/. --allow-root
-fi
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-  ./wp-cli.phar config create --path=/var/www/html/. --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-fi
+./wp-cli.phar core download --path=/var/www/html/. --allow-root
+  sleep 10
 
-./wp-cli.phar core install --path=/var/www/html/. --url=lsouquie.42.fr --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+# ./wp-cli.phar db create --path=/var/www/html --allow-root
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+# if [ ! -f /var/www/html/wp-config.php ]; then
+  ./wp-cli.phar config create --path=/var/www/html --dbname=$DB_NAME \ 
+  --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOSTNAME --allow-root
+# fi
+
+  ./wp-cli.phar core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE \
+  --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+  --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+
+  ./wp-cli.phar user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author \
+  --user_pass=$WORDPRESS_USER_PASSWORD --allow-root --path=/var/www/html
+
+
 
 # Lancer php-fpm
 exec php-fpm7.4 -F
